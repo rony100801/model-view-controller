feeaefa56f18a581ad012bde6961b4435e46c854
Rony Setiawan <setiawanrony603@gmail.com>
2022-07-11 21:38:08 -0700

    Menjalankan Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+    public function index($nama = "Rony", $hobi = "Bola")
+    {
+        echo "Halo nama saya $nama, dan hobi saya adalah $hobi";
+    }
+
+    public function page()
+    {
+        echo "About/page";
+    }
+}
